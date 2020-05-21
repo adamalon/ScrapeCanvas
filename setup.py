6907fc8e4b146a992b1563b737d3db0e53e316e1
Adam Alon <AdamAlonIL@gmail.com>
2020-05-21 22:32:24 +0300

    updated install file

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
 setup(
-  name = 'CanvasScraper',         # How you named your package folder
-  packages = ['CanvasScraper'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  name = 'ScrapeCanvas',         # How you named your package folder
+  packages = ['ScrapeCanvas'],   # Chose the same as "name"
+  version = '1.1',      # Start with a small number and increase it with every change you make
   license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python Scraper for Canvas',   # Give a short description about your library
   author = 'Adam Alon',                   # Type in your name
   author_email = 'adamalonil+canvasscraper@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/adamalon/CanvasScraper',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/adamalon/CanvasScraper/archive/v1.0.tar.gz',    # I explain this later on
-  keywords = ['Canvas', 'Scraper', 'MIT'],   # Keywords that define your package best
+  download_url = 'https://github.com/adamalon/CanvasScraper/archive/v1.1.tar.gz',    # I explain this later on
+  keywords = ['Canvas', 'Scraper', 'MIT', 'ScrapeCanvas'],   # Keywords that define your package best
   install_requires=[            
           'pyppeteer',
       ],
